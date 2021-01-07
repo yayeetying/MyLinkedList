17f6808601b81c02c0aa4b406b59e48981770fe9
Yaying <yliangli20@stuy.edu>
2021-01-06 22:29:20 -0500

    tested out a linkedlist

---
+++ Tester.java
@@ -0,0 +1,11 @@
+public class Tester {
+
+  public static void main(String[] args) {
+    MyLinkedList listo = new MyLinkedList();
+    System.out.println(listo.size());
+    System.out.println(listo.add("REE"));
+    System.out.println(listo.add("cuz"));
+    System.out.println(listo.add("dog"));
+  }
+
+}
